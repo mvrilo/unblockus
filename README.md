c97b6305e96f5c7a6a02cd3a12b0f97ea864f038
Murilo Santana <mvrilo@gmail.com>
2014-11-16 10:50:44 -0200

    info and license

---
+++ README.md
@@ -0,0 +1,15 @@
+# unblockus
+
+Command line utility for [unblockus](http://www.unblock-us.com/)
+
+```
+$ ./unblockus
+unblockus - cli interface for the unblock-us service
+
+Usage:
+  unblockus <email> [us|ar|at|be|br|ca|co|dk|fi|fr|de|ie|lu|mx|nl|no|se|ch|uk]
+
+Examples:
+  unblockus email@example.com # will simply reactivate the ip with the email
+  unblockus he@example.com ie # will reactivate and change the country to Ireland
+```
--- unblockus.go
+++ unblockus.go
@@ -1,3 +1,21 @@
+/*
+unblockus
+Copyright (C) 2014, Murilo Santana <mvrilo@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -10,7 +28,7 @@ import (
 	"sync"
 )
 
-const USAGE = `unblockus - cli interface for the unblock-us service
+const USAGE = `unblockus - command line interface for the unblock-us service
 
 Usage:
   unblockus <email> [%s]
